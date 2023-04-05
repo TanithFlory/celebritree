5ca1324c30b8881487c1581c966b95813df85424
Tanith Flory <tanithflory7770@gmail.com>
2023-04-05 20:14:53 +0530

    fix undefined causing crash

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -8,66 +8,99 @@ const createTransport = async () => {
   const clientSecret = process.env.OAUTH_CLIENT_SECRET;
   const clientID = process.env.OAUTH_CLIENTID;
   const refreshToken = process.env.OAUTH_REFRESH_TKN;
-  try {
-    const client = new OAuth2(
-      clientID,
-      clientSecret,
-      "https://developers.google.com/oauthplayground"
-    );
-    client.setCredentials({
-      refresh_token: refreshToken,
+  const client = new OAuth2(
+    clientID,
+    clientSecret,
+    "https://developers.google.com/oauthplayground"
+  );
+  client.setCredentials({
+    refresh_token: refreshToken,
+  });
+  const accessToken = await new Promise((resolve, reject) => {
+    client.getAccessToken((err, res) => {
+      if (err) {
+        console.log(err);
+        reject(new Error("Error, failed to generate token."));
+      }
+      resolve(res);
     });
-    const accessToken = await new Promise((resolve, reject) => {
-      client.getAccessToken((err, res) => {
-        if (err) {
-          reject(new Error("Error, failed to generate token."));
-        }
-        resolve(res);
-      });
-    });
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        accessToken,
-        type: "OAuth2",
-        user: email,
-        clientId: clientID,
-        clientSecret: clientSecret,
-        refreshToken: refreshToken,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    });
-    return transporter;
-  } catch (err) {
-    console.log(err);
-  }
+  });
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      accessToken,
+      type: "OAuth2",
+      user: email,
+      clientId: clientID,
+      clientSecret: clientSecret,
+      refreshToken: refreshToken,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+  return transporter;
 };
 
 const emailOtp = async (otp, email) => {
-  const transporter = await createTransport();
-  transporter.sendMail({
-    subject: "OTP Verification - Celebritree",
-    text: "Here is your one time password ",
-    to: email,
-    from: process.env.REACT_APP_EMAIL,
-    html: `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
-    <div style="margin:50px auto;width:70%;padding:20px 0">
-      <div style="border-bottom:1px solid #eee">
-        <a href="" style="font-size:1.4em;color: #53c24d;text-decoration:none;font-weight:600">Celebritree</a>
+  try {
+    const transporter = await createTransport();
+    transporter.sendMail({
+      subject: "OTP Verification - Celebritree",
+      text: "OTP",
+      to: email,
+      from: process.env.REACT_APP_EMAIL,
+      html: `        <div
+      style="
+        font-family: Franklin Gothic;
+        max-width: 550px;
+        margin: auto;
+      "
+    >
+      <div>
+        <img src="background.png" style="width: 100%; height: 250px" />
       </div>
-      <p style="font-size:1.1em">Hi,</p>
-      <p><b>Login OTP<b></p>
-      <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
-      <p style="font-size:0.9em;">Regards,<br />Celebritree</p>
-      <hr style="border:none;border-top:1px solid #eee" />
-      <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-        <p>Celebritree</p>
-        <p>Mangalore, INDIA</p>
+      <div style="color: #7b7b7b">
+        <h3 style="color: black">Hello,</h3>
+        <p>Verify your email address.</p>
+        <p>
+          Thank you for your interest! Here is your one time password. This code
+          is only valid for 10minutes.
+        </p>
+        <div style="display: flex">
+          <h2
+            style="
+              text-align: center;
+              padding: 10px;
+              background-color: black;
+              color: white;
+              border-radius: 5px;
+              display: inline-block;
+              margin: auto;
+            "
+          >
+            ${otp}
+          </h2>
+        </div>
+        <hr style="width: 100%; height: 2px; border: 0" />
+        <div style="display: flex">
+          <div style="text-align: center;">
+            <p style="margin-bottom: 3px">Thank you,</p>
+            <p style="margin: 0 0 3px">Regards,</p>
+            <p style="margin: 0; color: green">Celebritree</p>
+          </div>
+          <div style="margin: 0.5rem 0 0  14rem; text-align: center">
+            <p style="margin-bottom: 3px">
+              Please do not reply directly to this email.
+            </p>
+            <p style="margin: 0">You can contact us on help@celebritree.org</p>
+          </div>
+        </div>
       </div>
-    </div>
-   </div>`,
-  });
+    </div>`,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 export default emailOtp;
