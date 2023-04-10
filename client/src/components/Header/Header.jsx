4294717b24072884ce512e521e1e2f8c23df8ea0
Tanith Flory <tanithflory7770@gmail.com>
2023-04-10 10:49:06 +0530

    minor styling

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
             initial={{ x: 1000, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.3 }}
-            className="header__svg"
+            className="header__bg"
           >
             <img src={images.mainTree} alt="" />
             <div className="animated_header-wrapper">
